4c7790d235cef37c9fa885850e9f731d4e0fe49e
jeremyrdavis <jeremyrdavis@mac.com>
2023-12-02 06:23:47 -0500

    initial service working

--- shootout-cqrs/src/main/java/io/arrogantprogrammer/GreetingResource.java
+++ shootout-cqrs/src/main/java/io/arrogantprogrammer/GreetingResource.java
@@ -1,32 +1,23 @@
 package io.arrogantprogrammer;
 
+import io.arrogantprogrammer.proto.GreetingProto;
+import io.arrogantprogrammer.proto.ProtoGreetingService;
+import io.quarkus.grpc.GrpcService;
+import io.smallrye.mutiny.Uni;
 import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
-import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.MediaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
-@Path("/greetings")
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
-public class GreetingResource {
+@GrpcService
+public class GreetingResource implements ProtoGreetingService {
 
     static final Logger LOGGER = LoggerFactory.getLogger(GreetingResource.class);
 
     @Inject
     GreetingService greetingService;
 
-    @POST
-    @Transactional
-    public void addGreeting(GreetingJSON greetingJSON) {
-        LOGGER.debug("addGreeting: {}", greetingJSON);
-        greetingService.addGreeting(greetingJSON);
-    }
-
-    @GET
     public List<GreetingJSON> listGreetings() {
         LOGGER.debug("listGreetings");
         List<Greeting> allGreetings = Greeting.listAll();
@@ -34,5 +25,11 @@ public class GreetingResource {
         List<GreetingJSON> greetings = allGreetings.stream().map(greeting -> new GreetingJSON(greeting.text)).toList();
         LOGGER.debug("listGreetings returning {} greetings", greetings.size());
         return greetings;
+    }
+
+    @Override
+    public Uni<GreetingProto> addGreeting(GreetingProto request) {
+        LOGGER.debug("adding : {}", request);
+        return Uni.createFrom().item(request);
     }
 }
